7cae041905bd43446a244bd20683451e939900f4
Alexis Rodriguez <alexisrodriguezarteaga@gmail.com>
2024-11-21 12:38:06 -0300

    Replace deleted characters with michis

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PPS
 
 Prácticas Profesionales Supervisadas
+
+## Dudas
+
+¿Si se inserta caracteres en medio de una palabra, cuenta como error?
--- scripts/anonimization.py
+++ scripts/anonimization.py
@@ -15,7 +15,7 @@ def es_nombre(texto):
             return True
     return False
 
-def obtener_nombre_ficticio_con_espacios(texto):
+def obtener_input_anonimizado_con_espacios(texto):
     palabras = texto.split()  # Dividir por espacios
     palabras_ficticias = []
     for palabra in palabras:
@@ -23,13 +23,22 @@ def obtener_nombre_ficticio_con_espacios(texto):
         palabras_ficticias.append(palabra_ficticia)
     return ' '.join(palabras_ficticias)  # Reconstruir el texto con los espacios originales
 
-def anonimizar_subinputs(logs, input, input_text, logsModificados):
+def anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados):
+    errors = []
     for log in logs:
-        log_text = log["values"]["data"]["text"]
-        start_index = input_text.find(log_text)
-        if start_index != -1:
-            end_index = start_index + len(log_text)
-            log["values"]["data"]["text"] = input["values"]["data"]["text"][start_index:end_index]
+        start_index = 0
+        end_index = len(log["values"]["data"]["text"])
+        # Convertir la cadena en una lista para modificar por índice
+        log_text_lista = list(log["values"]["data"]["text"])
+        input_text_lista = list(input_text)
+        # Reemplazar las posiciones en las que difiere el caracter de log comparado al subinput correspondiente
+        for i in range(min(len(log_text_lista), len(input_text_lista))):  # Iterar hasta el mínimo de los dos
+            if log_text_lista[i] != input_text_lista[i]:
+                print(f"Error en la posición {i}: {log_text_lista[i]} != {input_text_lista[i]}")
+                errors.append(i)
+        # Convertir la lista de nuevo en una cadena
+        sub_input_anonimizado = input_anonimizado[start_index:end_index]
+        log["values"]["data"]["text"] = reemplazar_errores_con_michis(sub_input_anonimizado, errors)
         logsModificados.append(log)
     obj = {
         "type": "inputEnd",
@@ -37,11 +46,11 @@ def anonimizar_subinputs(logs, input, input_text, logsModificados):
     }
     logsModificados.append(obj)
 
-def reemplazar_errores_con_asteriscos(texto, posiciones_errores):
+def reemplazar_errores_con_michis(texto, posiciones_errores):
     texto_lista = list(texto)  # Convertir a lista para modificar por índice
     for pos in posiciones_errores:
         if pos < len(texto_lista):
-            texto_lista[pos] = '*'
+            texto_lista[pos] = '#'
     return ''.join(texto_lista)
 
 def execute():
@@ -73,49 +82,33 @@ def execute():
         max_length = -1
         input = None
         input_text = ''
-        error_positions = []  # Lista para almacenar posiciones de errores
 
         for previousEvent, nextEvent in zip(previous, next):
-            if previousEvent.get("type"):
+            if previousEvent.get("type") == "inputEnd":
+                print(f"previousEvent: {previousEvent['type']}")
                 if input is not None:
                     if es_nombre(input_text):
                         print(f"El texto '{input_text}' es un nombre.")
-                        nombre_ficticio = obtener_nombre_ficticio_con_espacios(input_text)
-                        input["values"]["data"]["text"] = nombre_ficticio
-                        input["values"]["data"]["text"] = reemplazar_errores_con_asteriscos(input["values"]["data"]["text"], error_positions)
-                        print(f"El nombre '{input_text}' ha sido anonimizado como '{input["values"]["data"]["text"]}'.")
-                        error_positions = []  # Resetear posiciones de errores
-                        anonimizar_subinputs(logs, input, input_text, logsModificados)
-                    # elif input_text.isdigit():
-                    #     primera_parte = input_text[0:int((len(input_text) / 2))]
-                    #     segunda_parte = "".join(["*" for _ in input_text[int(len(input_text) / 2):]])
-                    #     if "values" in input:
-                    #         input["values"]["data"]["text"] = primera_parte + segunda_parte
-                    #     anonimizar_subinputs(logs, input, input_text, logsModificados)
-
-                    # Reemplazar caracteres en las posiciones de errores con asteriscos
+                        input_anonimizado = obtener_input_anonimizado_con_espacios(input_text)
+                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados)
+                        print(f"El nombre '{input_text}' ha sido anonimizado como '{input_anonimizado}'.")
+                    elif input_text.isdigit():
+                        primera_parte = input_text[0:int((len(input_text) / 2))]
+                        segunda_parte = "".join(["*" for _ in input_text[int(len(input_text) / 2):]])
+                        input_anonimizado = primera_parte + segunda_parte
+                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados)
 
                 input = nextEvent
-
-                if "values" in nextEvent and "data" in nextEvent["values"] and "text" in nextEvent["values"]["data"]:
-                    max_length = len(nextEvent["values"]["data"]["text"])
+                max_length = len(nextEvent["values"]["data"]["text"])
+                input_text = nextEvent["values"]["data"]["text"]
                 logs = []
 
             else:
-                print("previousEvent", previousEvent["values"]["data"]["text"])
-                if "values" in previousEvent and "data" in previousEvent["values"] and "text" in previousEvent["values"]["data"]:
-                    if len(previousEvent["values"]["data"]["text"]) > max_length:
-                        input = previousEvent
-                        input_text = previousEvent["values"]["data"]["text"]
-                        max_length = len(previousEvent["values"]["data"]["text"])
-
-                    # Detectar errores en la escritura y agregar posición
-                    if "values" in nextEvent and "data" in nextEvent["values"] and "text" in nextEvent["values"]["data"]:
-                        if (len(previousEvent["values"]["data"]["text"]) > len(nextEvent["values"]["data"]["text"])):
-                            error_positions.append(len(previousEvent["values"]["data"]["text"]) - 1)
-
-                if "values" in previousEvent and "data" in previousEvent["values"] and "text" in previousEvent["values"]["data"]:
-                    logs.append(previousEvent)
+                if len(previousEvent["values"]["data"]["text"]) >= max_length:
+                    input = previousEvent
+                    input_text = previousEvent["values"]["data"]["text"]
+                    max_length = len(previousEvent["values"]["data"]["text"])
+                logs.append(previousEvent)
 
         cast['logs'] = logsModificados
         logsModificados = []
