f95875052069b95b2e825968eed56220faac57ad
Alexis Rodriguez <alexisrodriguezarteaga@gmail.com>
2024-12-10 11:10:53 -0300

    Fix Errors

--- scripts/anonymize.py
+++ scripts/anonymize.py
@@ -28,22 +28,19 @@ def obtener_input_anonimizado_con_espacios(texto):
         palabras_ficticias.append(palabra_ficticia)
     return ' '.join(palabras_ficticias)  # Reconstruir el texto con los espacios originales
 
-def anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados):
-    errors = []
-    for log in logs:
+def anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados, errors):
+    for i in range(len(logs)):
+        log = logs[i]
         start_index = 0
         end_index = len(log["values"]["data"]["text"])
         # Convertir la cadena en una lista para modificar por índice
         log_text_lista = list(log["values"]["data"]["text"])
         input_text_lista = list(input_text)
         # Reemplazar las posiciones en las que difiere el caracter de log comparado al subinput correspondiente
-        for i in range(min(len(log_text_lista), len(input_text_lista))):  # Iterar hasta el mínimo de los dos
-            if log_text_lista[i] != input_text_lista[i]:
-                errors.append(i)
-        # Convertir la lista de nuevo en una cadena
         sub_input_anonimizado = input_anonimizado[start_index:end_index]
         if len(input_anonimizado) != len(log["values"]["data"]["text"]):
-            log["values"]["data"]["text"] = reemplazar_errores_con_michis(sub_input_anonimizado, errors)
+            print(f"tamanio array errores {len(errors)}")
+            log["values"]["data"]["text"] = reemplazar_errores_con_michis(sub_input_anonimizado, errors[i])
         else:
             log["values"]["data"]["text"] = sub_input_anonimizado
         logsModificados.append(log)
@@ -52,6 +49,31 @@ def anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModific
         "timestamp": input["values"]["timestamp"] + 1
     }
     logsModificados.append(obj)
+
+def detectar_errores(logs, position, errors):
+    # for i in range(len(logs)):
+    log = logs[position]
+
+    # Reemplazar las posiciones en las que difiere el caracter de log comparado al subinput correspondiente
+    logsAnteriores = logs[:position][::-1]  # Obtener los logs anteriores en orden inverso
+
+    # Buscar solo el log anterior más relevante
+    log_referencia = None
+    for j in range(len(logsAnteriores)):
+        if len(log["values"]["data"]["text"]) == len(logsAnteriores[j]["values"]["data"]["text"]):
+            log_referencia = logsAnteriores[j]
+            break  # Solo el primer match
+
+    # Si existe un log previo para comparar, detecta los errores
+    if log_referencia:
+        for k in range(len(log["values"]["data"]["text"])):
+            if log["values"]["data"]["text"][k] != log_referencia["values"]["data"]["text"][k]:
+                print(f"caracter {log["values"]["data"]["text"][k]} != {log_referencia["values"]["data"]["text"][k]}")
+                print(f"input con errores {log["values"]["data"]["text"]}")
+                print(f"input antes de los errores {log_referencia["values"]["data"]["text"]}")
+                print(f"position {position}")
+                print(f"tamanio array errores {len(errors)}")
+                errors[position].append(k)
 
 def reemplazar_errores_con_michis(texto, posiciones_errores):
     texto_lista = list(texto)  # Convertir a lista para modificar por índice
@@ -90,26 +112,26 @@ def execute():
         input = None
         input_text = ''
 
-        errores = []
-
         for previousEvent, nextEvent in zip(previous, next):
             if previousEvent.get("type") == "inputEnd":
-                # print(f"previousEvent: {previousEvent['type']}")
+                errors = [[] for _ in range(999)]
+                for i in range(len(logs)):
+                    detectar_errores(logs, i, errors)
                 if input is not None:
                     if is_email(input_text):
                         primera_parte = input_text[0:input_text.index('@')]
                         input_anonimizado = obtener_input_anonimizado_con_espacios(primera_parte)
                         input_anonimizado += input_text[input_text.index('@'):]
-                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados)
+                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados, errors)
                     elif input_text.isdigit():
                         primera_parte = input_text[0:int((len(input_text) / 2))]
                         segunda_parte = "".join(["*" for _ in input_text[int(len(input_text) / 2):]])
                         input_anonimizado = primera_parte + segunda_parte
-                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados)
+                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados, errors)
                     elif es_nombre(input_text):
                         print(f"El texto '{input_text}' es un nombre.")
                         input_anonimizado = obtener_input_anonimizado_con_espacios(input_text)
-                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados)
+                        anonimizar_subinputs(logs, input, input_text, input_anonimizado, logsModificados, errors)
                 input = nextEvent
                 max_length = len(nextEvent["values"]["data"]["text"])
                 input_text = nextEvent["values"]["data"]["text"]
